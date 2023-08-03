44c95853f30810ef1c2edff280f70366741005fe
calvin22580 <hollowaycalvin@gmail.com>
2023-08-03 15:08:59 +0100

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -6,20 +6,21 @@ async function getReply() {
     userText.textContent = 'User: ' + input;
     chatbox.appendChild(userText);
 
-    let response = await fetch('https://chatbot-fvrxq.northeurope.inference.ml.azure.com/score', {
+    let response = await fetch('https://chatbot-uojyc.northeurope.inference.ml.azure.com/score', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': 'Bearer <NoamZjwQX5gWz4yfElEOZfnt6B6I2Nfk>',
         },
         body: JSON.stringify({
-            'query': input,
+            'question': input,
         }),
     });
 
     let data = await response.json();
-    
+
     let botText = document.createElement('p');
-    botText.textContent = 'Bot: ' + data.response;
+    botText.textContent = 'Bot: ' + data.answer;
     chatbox.appendChild(botText);
 
     // Automatically scroll to the bottom of the chatbox
