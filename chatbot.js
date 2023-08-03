94e411f221004ba094958efe29d8d488842d3927
calvin22580 <hollowaycalvin@gmail.com>
2023-08-03 15:14:00 +0100

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -6,22 +6,30 @@ async function getReply() {
     userText.textContent = 'User: ' + input;
     chatbox.appendChild(userText);
 
-    let response = await fetch('https://chatbot-uojyc.northeurope.inference.ml.azure.com/score', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer NoamZjwQX5gWz4yfElEOZfnt6B6I2Nfk',
-        },
-        body: JSON.stringify({
-            'question': input,
-        }),
-    });
+    try {
+        let response = await fetch('https://chatbot-uojyc.northeurope.inference.ml.azure.com/score', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'NoamZjwQX5gWz4yfElEOZfnt6B6I2Nfk',
+            },
+            body: JSON.stringify({
+                'question': input,
+            }),
+        });
 
-    let data = await response.json();
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
-    let botText = document.createElement('p');
-    botText.textContent = 'Bot: ' + data.answer;
-    chatbox.appendChild(botText);
+        let data = await response.json();
+
+        let botText = document.createElement('p');
+        botText.textContent = 'Bot: ' + data.answer;
+        chatbox.appendChild(botText);
+    } catch(e) {
+        console.error('An error occurred:', e);
+    }
 
     // Automatically scroll to the bottom of the chatbox
     chatbox.scrollTop = chatbox.scrollHeight;
